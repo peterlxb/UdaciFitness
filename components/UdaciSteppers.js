bc8d4f1f876ecdcf79d0ff0acf76fec0b5c5192a
peterliuxb <woairuidaren@gmail.com>
2017-09-22 22:20:24 +0800

    Changes for style-Steppers

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,24 +1,62 @@
 import React,{Component} from 'react'
-import {View,Text,TouchableOpacity} from 'react-native'
+import {View,Text,TouchableOpacity,Platform,StyleSheet} from 'react-native'
 import { FontAwesome ,entypo } from '@expo/vector-icons'
 
 export default function UdaciSteppers ({max, unit, step, value, onIncrement,onDecrement}) {
   return(
-    <View>
-      <View>
-        <TouchableOpacity onPress={onDecrement}>
-          <FontAwesome name='minus' size={30} color={'black'} />
-        </TouchableOpacity>
+    <View style={[styles.row,{justifyContent:'space-between'}]}>
+      {Platform.os === 'ios'
+        ? <View style={{flexDirection:'row'}}>
+            <TouchableOpacity onPress={onDecrement}
+              style={[styles.iosBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
+              <FontAwesome name='minus' size={30} color={'purple'} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onIncrement}
+              style={[styles.iosBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0, borderLeftWidth: 0}]}>
+              <FontAwesome name='plus' size={30} color={'purple'} />
+            </TouchableOpacity>
+          </View>
 
-        <TouchableOpacity onPress={onIncrement}>
-          <FontAwesome name='plus' size={30} color={'black'} />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+        :  <View style={{flexDirection:'row'}}>
+            <TouchableOpacity onPress={onDecrement} style={styles.androidBtn}>
+              <FontAwesome name='minus' size={30} color={'white'} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onIncrement} style={styles.androidBtn}>
+              <FontAwesome name='plus' size={30} color={'white'} />
+            </TouchableOpacity>
+          </View>}
+          <View style={styles.metricCounter}>
+            <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+            <Text style={{fontSize: 18, color: 'gray'}}>{unit}</Text>
+          </View>
 
-      </View>
+
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection:'row',
+    flex:1,
+    alignItems:'center'
+  },
+  androidBtn: {
+    margin:5,
+    backgroundColor:'purple',
+    padding:10,
+    borderRadius:2
+  },
+  isoBtn: {
+    backgroundColor:'white',
+    borderColor:'purple',
+    borderWidth:1,
+    padding:5
+  },
+  metricCounter: {
+    width:85,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
