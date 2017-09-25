d5fbfb631b7730c8d15548001a5ef0c612b6a8fc
peterliuxb <woairuidaren@gmail.com>
2017-09-25 16:31:49 +0800

    Changes for StatusBar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View ,Platform} from 'react-native';
+import { View ,Platform,StatusBar} from 'react-native';
 import AddEntry from './components/AddEntry'
 import History from './components/History'
 import { Provider } from 'react-redux'
@@ -7,6 +7,15 @@ import { createStore } from 'redux'
 import reducer  from './reducers'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { TabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+
+function UdaciStatusBar({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor , height:Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
 
 const Tabs = TabNavigator({
   History: {
@@ -45,6 +54,7 @@ export default class App extends Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex:1}}>
+          <UdaciStatusBar backgroundColor={'purple'} barStyle='light-content' />
           <Tabs />
         </View>
       </Provider>
